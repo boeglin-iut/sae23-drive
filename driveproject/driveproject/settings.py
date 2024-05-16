ff9f27cee64d7f42a66f9ee28e7e4750235a6861
simonIUT68 <147594610+simoniut68@users.noreply.github.com>
2024-05-16 15:01:08 +0200

    ajout de la base de donnée externe au projet django

--- driveproject/driveproject/settings.py
+++ driveproject/driveproject/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'driveproject.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'django',
+        'USER': 'django',
+        'PASSWORD': 'django',
+        'HOST': '10.128.200.12',
+        'PORT': '3306',
     }
 }
 
