7c69f420e0308c57ffc6926c04cb0cbd9e22d089
Xurron <corentin.boeglin2005@gmail.com>
2024-05-17 16:10:21 +0200

    ajout d'un premier jet d'un CRUD pour l'ensemble du projet et chargement de la base de donnée existante

--- driveproject/drive/models.py
+++ driveproject/drive/models.py
@@ -1,3 +1,64 @@
+# This is an auto-generated Django model module.
+# You'll have to do the following manually to clean this up:
+#   * Rearrange models' order
+#   * Make sure each model has one field with primary_key=True
+#   * Make sure each ForeignKey and OneToOneField has `on_delete` set to the desired behavior
+#   * Remove `managed = False` lines if you wish to allow Django to create, modify, and delete the table
+# Feel free to rename the models, but don't rename db_table values or field names.
 from django.db import models
 
-# Create your models here.
+
+class Categorie(models.Model):
+    id_cat = models.IntegerField(primary_key=True)
+    nom = models.CharField(max_length=50, blank=True, null=True)
+    descriptif = models.CharField(max_length=50, blank=True, null=True)
+
+    class Meta:
+        managed = False
+        db_table = 'categories'
+
+
+class Clients(models.Model):
+    id_client = models.IntegerField(primary_key=True)
+    nom = models.CharField(max_length=50, blank=True, null=True)
+    prenom = models.CharField(max_length=50, blank=True, null=True)
+    date_inscription = models.DateField(blank=True, null=True)
+    adresse = models.CharField(max_length=100, blank=True, null=True)
+
+    class Meta:
+        managed = False
+        db_table = 'clients'
+
+
+class Commandes(models.Model):
+    id_commande = models.IntegerField(primary_key=True)
+    id_client = models.ForeignKey(Clients, models.DO_NOTHING, db_column='id_client', blank=True, null=True)
+    date = models.DateField(blank=True, null=True)
+
+    class Meta:
+        managed = False
+        db_table = 'commandes'
+
+
+class ListeProduits(models.Model):
+    id_liste = models.IntegerField(primary_key=True)
+    quantite = models.IntegerField(blank=True, null=True)
+    id_produit = models.ForeignKey('Produit', models.DO_NOTHING, db_column='id_produit', blank=True, null=True)
+    id_commande = models.ForeignKey(Commandes, models.DO_NOTHING, db_column='id_commande', blank=True, null=True)
+
+    class Meta:
+        managed = False
+        db_table = 'liste_produits'
+
+
+class Produit(models.Model):
+    id_produit = models.IntegerField(primary_key=True)
+    nom = models.CharField(max_length=50, blank=True, null=True)
+    date_de_peremption = models.DateField(blank=True, null=True)
+    marque = models.CharField(max_length=50, blank=True, null=True)
+    prix = models.DecimalField(max_digits=10, decimal_places=2, blank=True, null=True)
+    id_categorie = models.ForeignKey(Categorie, models.DO_NOTHING, db_column='id_categorie', blank=True, null=True)
+
+    class Meta:
+        managed = False
+        db_table = 'produits'
--- driveproject/drive/views.py
+++ driveproject/drive/views.py
@@ -2,3 +2,63 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'drive/index.html')
+
+def categories(request):
+    return render(request, 'drive/categories/categories.html')
+
+def categories_add(request):
+    return render(request, 'drive/categories/categories_add.html')
+
+def categories_edit(request, id):
+    return render(request, 'drive/categories/categories_edit.html')
+
+def categories_delete(request, id):
+    return render(request, 'drive/categories/categories_delete.html')
+
+def clients(request):
+    return render(request, 'drive/clients/commandes.html')
+
+def clients_add(request):
+    return render(request, 'drive/clients/clients_add.html')
+
+def clients_edit(request, id):
+    return render(request, 'drive/clients/clients_edit.html')
+
+def clients_delete(request, id):
+    return render(request, 'drive/clients/clients_delete.html')
+
+def commandes(request):
+    return render(request, 'drive/commandes/commandes.html')
+
+def commandes_add(request):
+    return render(request, 'drive/commandes/commandes_add.html')
+
+def commandes_edit(request, id):
+    return render(request, 'drive/commandes/commandes_edit.html')
+
+def commandes_delete(request, id):
+    return render(request, 'drive/commandes/commandes_delete.html')
+
+def liste_produits(request):
+    return render(request, 'drive/liste_produits/liste_produits.html')
+
+def liste_produits_add(request):
+    return render(request, 'drive/liste_produits/liste_produits_add.html')
+
+def liste_produits_edit(request, id):
+    return render(request, 'drive/liste_produits/liste_produits_edit.html')
+
+def liste_produits_delete(request, id):
+    return render(request, 'drive/liste_produits/liste_produits_delete.html')
+
+def produits(request):
+    return render(request, 'drive/produits/produits.html')
+
+def produits_add(request):
+    return render(request, 'drive/produits/produits_add.html')
+
+def produits_edit(request, id):
+    return render(request, 'drive/produits/produits_edit.html')
+
+def produits_delete(request, id):
+    return render(request, 'drive/produits/produits_delete.html')
